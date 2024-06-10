f95dff87b909c2323c49a9c58bd9501969e49991
ghazalmohseni7 <ghazalmohseni79@gmail.com>
2024-06-10 18:20:46 +0330

    looks like it works fine

--- client.py
+++ client.py
@@ -1,7 +1,34 @@
 from websocket import create_connection
-def stream_websocket_data():
+import json
+def stream_websocket_data(is_8_digit):
     ws = create_connection("ws://localhost:4001")
-    ws.send("face")
+    # ws.send("plate",1)
+    message={"command":"plate","is_8_digit":str(is_8_digit)}
+    print(message)
+    ws.send(json.dumps(message))
+    print('ssend')
     result =  ws.recv()
+    print(result)
+    x=json.loads(result)
+    print("????????????????////",len(x["data_plate"]))
+    #ws.close()
+# print(stream_websocket_data())
+def stream_websocket_parking_object(is_8_digit=True):
+    print("FFFFFFFFF",is_8_digit)
+    ws = create_connection("ws://localhost:4001")
+    message={"command":"parkingObject","is_8_digit":str(is_8_digit)}
+    ws.send(json.dumps(message))
+    result =  ws.recv()
+    x=json.loads(result)
+    print("????????????????////",x)
     ws.close()
-print(stream_websocket_data())
+if __name__=="__main__":
+    # stream_websocket_data(is_8_digit=True)
+    # stream_websocket_data(is_8_digit="k")
+    stream_websocket_parking_object()
+    # while(True):
+    #     x=stream_websocket_data()
+    #     if x :
+    #         exit
+    #     # print(type(x))
+    
--- server.py
+++ server.py
@@ -11,19 +11,48 @@ EventPlate = db_plate["event"]
 parking_object_collection=db_plate["parking_object"]
 
 class SimpleEcho(WebSocket):
+  
     def handle(self):
-        if self.data=='plate':
+        print(self.data , type(self.data))
+       
+        message=json.loads(self.data)
+        print(message)
+        print("WWWWWWWWWWWWWW")
+        # counter=0
+        if message["command"]=='plate' and message["is_8_digit"]=="True":
             for client in self.server.connections.values():
+                print("                printis_8_digit     True")
                 # print(list(EventPlate.find().sort('_id',DESCENDING).limit(5)))
                 # whole_data={"data_plate":list(EventPlate.find({},{'_id':0}).sort('_id',DESCENDING).limit(5)) ,
                 #  "data_confirmed":list(parking_object_collection.find({},{'_id':0}).sort('_id',DESCENDING).limit(5))}
                 # client.send_message(json.dumps(whole_data))
+                print(json.dumps({'data_plate':list(EventPlate.find({},{'_id':0}).sort('_id',DESCENDING).limit(5))}))
                 client.send_message(json.dumps({'data_plate':list(EventPlate.find({},{'_id':0}).sort('_id',DESCENDING).limit(5))}))
+                # print(counter)
+                # counter+=1
                 # client.send_message(json.dumps({'data_confirmed':list(parking_object_collection.find({},{'_id':0}).sort('_id',DESCENDING).limit(5))}))
-        if self.data=="parkingObject":
+        if message["command"]=='plate' and message["is_8_digit"]=="False":
+            print("                printis_8_digit     False")
+            for client in self.server.connections.values():
+                
+                print("now front you should make a request to api: ")
+                
+                client.send_message(json.dumps({"data_plate":"now front you should make a request to api: "}))
+        if message["command"]=='plate' and message["is_8_digit"]=="k":
+            for client in self.server.connections.values():
+                print("passsss")
+                client.send_message(json.dumps({"data_plate":"First time do not have answer"}))
+            
+                
+                # print("now front you should make a request to api: ")
+                
+                # client.send_message(json.dumps({"data_plate":"now front you should make a request to api: "}))
+        print("YYYYYYYYYYy")       
+        if message["command"]=="parkingObject":
+            print("**************************8")
             for client in self.server.connections.values():
                 client.send_message(json.dumps({'data_confirmed':list(parking_object_collection.find({},{'_id':0}).sort('_id',DESCENDING).limit(5))}))
-
+            print("77777777777777777777777")
         #if self.data!="parkingObject" or self.data!='plate' :
             #self.send_message(self.data)
     def connected(self):
